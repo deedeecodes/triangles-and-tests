c4115cfb8ab161d36dac6f24b969d167f9f87e4e
diana <dianetodirica@gmail.com>
2023-03-23 10:48:33 +0200

    Modify number printing

--- src/main/java/com/diane/Exercise.java
+++ src/main/java/com/diane/Exercise.java
@@ -5,6 +5,9 @@ public class Exercise {
     public static void printNumbers() {
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= 10; i++) {
+            if(i == 9) {
+                continue;
+            }
             sb.append(i + ", ");
         }
         String numbers = sb.toString();
--- src/test/java/com/diane/ExerciseTest.java
+++ src/test/java/com/diane/ExerciseTest.java
@@ -10,7 +10,7 @@ class ExerciseTest {
     @Test
     void printNumbers() throws Exception {
         String actual = tapSystemOut (Exercise::printNumbers);
-        assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10", actual.trim());
+        assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 10", actual.trim());
     }
 
     @Test
