961614204c0c49d8e92f81016717725e9b238d2e
diana <dianetodirica@gmail.com>
2023-03-23 10:08:23 +0200

    Add required functionality

---
+++ src/main/java/com/diane/Exercise.java
@@ -0,0 +1,17 @@
+package com.diane;
+
+public class Exercise {
+
+    public static void printNumbers() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= 10; i++) {
+            sb.append(i + ", ");
+        }
+        String numbers = sb.toString();
+        System.out.println(numbers.substring(0, numbers.length() - 2));
+    }
+
+    public static double triangleArea(double base, double height) {
+        return (height * base) / 2;
+    }
+}
---
+++ src/test/java/com/diane/ExerciseTest.java
@@ -0,0 +1,21 @@
+package com.diane;
+
+import org.junit.jupiter.api.Test;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExerciseTest {
+
+    @Test
+    void printNumbers() throws Exception {
+        String actual = tapSystemOut (Exercise::printNumbers);
+        assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10", actual.trim());
+    }
+
+    @Test
+    void triangleArea() {
+        double actual = Exercise.triangleArea(5, 8);
+        assertEquals(20, actual);
+    }
+}
